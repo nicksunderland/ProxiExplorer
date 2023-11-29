81ed083b2c49f6a677d9d706cd83fe98b3db2cbe
Nicholas Sunderland <nicholas.p.sunderland@gmail.com>
2023-11-29 03:04:08 +0000

    initial

--- R/app_server.R
+++ R/app_server.R
@@ -1,9 +1,170 @@
+# options(shiny.trace=TRUE)
+
+# silence R CMD checks
+BETA_exposure <- BETA_outcome <- BP <- BP_exposure <- CHR <- P <- P_cat <- NULL
+P_exposure <- RSID_exposure <- SE_exposure <- SE_outcome <- index <- NULL
+
 #' The application server-side
 #'
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
-#' @import shiny
+#' @import shiny ggplot2 ggrepel data.table genepi.utils
+#' @importFrom shinyjs disable enable
+#' @importFrom stats runif
+#' @importFrom forcats fct_rev
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+
+  # reactive values
+  exposure_filepath <- reactiveVal(NULL)
+  outcome_filepath  <- reactiveVal(NULL)
+  pfile             <- reactiveVal(NULL)
+  exposure_dat      <- reactiveVal(NULL)
+  outcome_dat       <- reactiveVal(NULL)
+  harmonised_dat    <- reactiveVal(NULL)
+
+  # observe the import data button
+  observeEvent(input$import, {
+    c <- input$gene_chr
+    s <- input$gene_start
+    e <- input$gene_end
+    f <- input$gene_flanks_kb * 1000
+
+    # file path checks
+    if(!file.exists(exposure_filepath())) return(NULL)
+    d <- genepi.utils::standardise_gwas(exposure_filepath(), input_format="default")
+    print(c)
+    print(s)
+    print(e)
+    print(f)
+    print(d)
+    # TODO: column and data checks here
+    d <- d[CHR==c & BP>=s-f & BP<=e+f, ]
+    # calculate needed things
+    d[, P_cat := cut(P, breaks=c(1,5e-2,5e-4,5e-5,5e-6,5e-8,5e-100))]
+    # set the data
+    exposure_dat(d)
+
+    # TABLE STRUCTURE:
+    #
+  })
+
+  # observe clump button
+  observeEvent(input$clump, {
+
+    # check data
+    if(is.null(exposure_dat())) return(NULL)
+    if(is.null(outcome_dat())) {
+      o <- genepi.utils::standardise_gwas(outcome_filepath(), input_format="default")
+      outcome_dat(o)
+    }
+
+    p1 <- input$clump_p1
+    p2 <- input$clump_p2
+    r2 <- input$clump_r2
+    kb <- input$clump_kb
+    plink2 <- system.file("resources", "plink2", package="ProxiExplorer")
+    plink_ref <- pfile()
+    clumped_dat <- genepi.utils::clump(exposure_dat(), p1, p2, r2, kb, plink2, plink_ref)
+    # harmonise with outcome
+    h <- harmonise(clumped_dat, outcome_dat(), gwas1_trait="exposure", gwas2_trait="outcome", merge=c("RSID"="RSID"))
+    # set the data
+    harmonised_dat(h)
+
+    # TABLE STRUCTURE:
+    #
+  })
+
+
+
+  # the main display output
+  output$locus_plot <- renderPlot({
+
+    # check data
+    if(is.null(exposure_dat())) return(NULL)
+
+    # create the locus plot
+    p <- ggplot(data    = exposure_dat(),
+                mapping = aes(x=BP, y=-log10(P))) +
+      geom_point(color="lightgray") +
+      annotate(geom = "rect", xmin=input$gene_start, xmax=input$gene_end, ymin=0, ymax=Inf, fill="blue", alpha = 0.05) +
+      theme_classic() +
+      labs(x     = paste0("Chromosome ", input$gene_chr, " position"),
+           y     = expression(paste("-log"[10], plain(P))),
+           subtitle = input$data_input)
+
+    # if there is clumped data, plot
+    if(!is.null(harmonised_dat())) {
+      p <- p +
+        geom_point(data = harmonised_dat()[!is.na(clump), ], mapping = aes(x=BP_exposure, y=-log10(P_exposure), color=clump, fill=clump), shape=23) +
+        geom_vline(data = harmonised_dat()[index==TRUE, ],   mapping = aes(xintercept = BP_exposure), linetype="dotted", color="darkred") +
+        geom_point(data = harmonised_dat()[index==TRUE, ],   mapping = aes(x=BP_exposure, y=-log10(P_exposure)), size=3, fill="red", color="red", shape=24) +
+        geom_label(data = harmonised_dat()[index==TRUE, ],   mapping = aes(label = clump, x = BP_exposure, y = -0.5)) +
+        geom_label_repel(data = harmonised_dat()[index==TRUE, ], mapping = aes(label = RSID_exposure, x = BP_exposure, y = -log10(P_exposure)))
+    }
+
+    # plot
+    return(p)
+  })
+
+
+  # plot the clump analysis
+  output$clumps_plot <- renderPlot({
+
+    # check data
+    if(is.null(harmonised_dat())) return(NULL)
+
+    # create the locus plot
+    p <- ggplot(data    = harmonised_dat(),
+                mapping = aes(x=BETA_exposure, y=BETA_outcome)) +
+      geom_point(aes(color=fct_rev(P_cat), fill=fct_rev(P_cat), alpha=fct_rev(P_cat))) +
+      geom_point(data = harmonised_dat()[index==TRUE, ], size=3, fill="red", color="red", shape=24) +
+      geom_smooth(method="lm", mapping = aes(weight = (1/SE_exposure)*(1/SE_outcome)), color="red") +
+      viridis::scale_fill_viridis(name="Exposure P-value", discrete=TRUE) +
+      viridis::scale_color_viridis(name="Exposure P-value", discrete=TRUE) +
+      scale_alpha_discrete(name="Exposure P-value") +
+      facet_wrap(~clump, ncol=6, scales = "free") +
+      labs(x = expression('\u03B2'[exposure]),
+           y = expression('\u03B2'[outcome]),
+           title = paste0("Gene clumps - ", input$data_input),
+           subtitle= "Regression line weighted by 1/SE")
+
+    # show
+    return(p)
+
+
+  })
+
+
+
+
+  # observing the data input select box
+  observeEvent(input$data_input, {
+    if(input$data_input == "Custom input") {
+
+      # set random data, activate file input and wait for input data
+      exposure_dat(data.table(CHR = rep("6", 10000), BP = sample(1:50000, 10000, replace = FALSE), P = runif(10000)))
+      outcome_dat(data.table(CHR = rep("6", 10000), BP = sample(1:50000, 10000, replace = FALSE), P = runif(10000)))
+      shinyjs::enable(id="exposure_file")
+      shinyjs::enable(id="outcome_file")
+
+    } else {
+
+      # use example dataset, disable file input
+      shinyjs::disable(id="exposure_file")
+      shinyjs::disable(id="outcome_file")
+
+      # assess which option was chosen
+      if(input$data_input == "GLP1R - HbA1c vs. HF") {
+
+        exposure_filepath(system.file("extdata", "hba1c_jurgens_2022_gwas.tsv.gz", package="ProxiExplorer"))
+        outcome_filepath(system.file("extdata", "heartfailure_shah_2020_gwas.tsv.gz", package="ProxiExplorer"))
+        pfile(sub(".pgen", "", system.file("extdata", "glp1r.pgen", package="ProxiExplorer")))
+
+      }
+      # other options
+
+    }
+  })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -3,17 +3,136 @@
 #' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom shinyjs useShinyjs
+#' @importFrom shinyWidgets prettyRadioButtons sliderTextInput
 #' @noRd
 app_ui <- function(request) {
-  tagList(
+  # Create the page content
+  fluidPage(
+    # custom css
+    tags$head(
+      tags$style(HTML("hr {border-top: 1px solid #000000;}"))
+    ),
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # Your application UI logic
-    fluidPage(
-      h1("ProxiExplorer")
-    )
-  )
-}
+    # use shinyjs (for element activation/deactivation things)
+    shinyjs::useShinyjs(),
+    # A title
+    titlePanel("ProxiExplorer"),
+    p("Nick Sunderland - nicholas.sunderland@bristol.ac.uk"),
+    # Horizontal line
+    tags$hr(),
+    # sidebar layout
+    sidebarLayout(position = "right",
+                  sidebarPanel(p(strong("Controls")),
+                               width = 3,
+                               hr(),
+                               fluidRow(
+                                 column(12, selectInput(inputId  = "data_input",
+                                                        label    = "Data input:",
+                                                        choices  = c("GLP1R - HbA1c vs. HF", "Custom input"),
+                                                        selected = "GLP1R - HbA1c vs. HF"))
+                               ),
+                               fluidRow(
+                                 column(6, fileInput(inputId = "exposure_file",
+                                                     label   = "Exposure:")),
+                                 column(6, fileInput(inputId = "outcome_file",
+                                                     label   = "Outcome:"))
+                               ),
+                               fluidRow(
+                                 column(6, actionButton(inputId = "import",
+                                                        label   = "Import data")),
+                                 column(6, actionButton(inputId = "clump",
+                                                        label   = "Clump data")),
+                               ),
+                               hr(),
+                               fluidRow(
+                                 column(6, p(strong("Gene region")))
+                               ),
+                               fluidRow(
+                                 column(6, selectInput(inputId  = "gene_chr",
+                                                       label    = "Chrom:",
+                                                       choices  = c(as.character(1:22),"X"),
+                                                       selected = "6")),
+                                 column(6, numericInput(inputId = "gene_flanks_kb",
+                                                        label   = "Flanks (kb):",
+                                                        value   = 250,
+                                                        min     = 0,
+                                                        max     = 1000))
+                               ),
+                               fluidRow(
+                                 column(6, numericInput(inputId = "gene_start",
+                                                        label   = "Start:",
+                                                        value   = 39016574,
+                                                        min     = 1,
+                                                        max     = 250000000,
+                                                        step    = 1000)),
+                                 column(6, numericInput(inputId = "gene_end",
+                                                        label   = "End:",
+                                                        value   = 39055519,
+                                                        min     = 1,
+                                                        max     = 250000000,
+                                                        step    = 1000))
+                               ),
+                               hr(),
+                               fluidRow(
+                                 column(6, p(strong("Clumping")))
+                               ),
+                               fluidRow(
+                                 column(6, sliderTextInput(inputId  = "clump_p1",
+                                                           label    = "p1",
+                                                           choices  = c(5e-8,5e-6,5e-4,0.05,0.01,0.1,0.5,1.0),
+                                                           selected = 5e-8,
+                                                           grid     = TRUE)),
+                                 column(6, sliderTextInput(inputId  = "clump_p2",
+                                                           label    = "p2",
+                                                           choices  = c(5e-8,5e-6,5e-4,0.05,0.01,0.1,0.5,1.0),
+                                                           selected = 1.0,
+                                                           grid     = TRUE))
+                               ),
+                               fluidRow(
+                                 column(6, sliderTextInput(inputId  = "clump_r2",
+                                                           label    = "r2",
+                                                           choices  = c(0.0001,0.001,0.01,seq(0.1,1,0.1)),
+                                                           selected = 0.001,
+                                                           grid     = TRUE)),
+                                 column(6, sliderTextInput(inputId  = "clump_kb",
+                                                           label    = "kb",
+                                                           choices  = seq(0,1000,50),
+                                                           selected = 250,
+                                                           grid     = TRUE))
+                               ),
+                               hr(),
+                               fluidRow(
+                                 column(12, p(strong("Mendelian Randomisation")))
+                               ),
+                               fluidRow(
+                                 column(12, prettyRadioButtons(inputId      = "mr_method",
+                                                               label        = "Method:",
+                                                               choiceNames  = c("IVW", "IVW-LDcorr"),
+                                                               choiceValues = c("ivw", "ivw_ldcorr"),
+                                                               selected     = "ivw",
+                                                               inline       = TRUE,
+                                                               animation    = "smooth")
+                                 )
+                               )
+                    ), # sidebar panel end
+                    mainPanel(p(strong("Locus plot:")),
+                              width = 9,
+                              plotOutput(outputId = "locus_plot",
+                                         height   = "400px"),
+                              p(strong("Clumps:")),
+                              plotOutput(outputId = "clumps_plot",
+                                         height   = "400px")
+                    ) # main panel end
+      ) # sidebar layout end
+    ) # fluid page end
+} # ui function end
+
+
+
+
+
 
 #' Add external Resources to the Application
 #'
